9952351bace596c5dec75ffb55bc935bf4b2dc9f
carfox <carlotreball@gmail.com>
2023-12-06 00:01:21 -0500

    add trabajo en clase y boton activity

--- 02-android/app/src/main/java/com/example/b2023_gr1sw_cjlc/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/b2023_gr1sw_cjlc/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.b2023_gr1sw_cjlc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,11 +13,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.appcompat.app.AppCompatActivity
+import com.example.b2023_gr1sw_cjlc.ui.theme.ACicloVida
 import com.example.b2023_gr1sw_cjlc.ui.theme.B2023gr1swcjlcTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+            botonCicloVida.
+                setOnClickListener{
+                    irActividad(ACicloVida::class.java)
+                }
         setContent {
             B2023gr1swcjlcTheme {
                 // A surface container using the 'background' color from the theme
@@ -28,6 +37,12 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    fun irActividad( clase: Class<*>){
+        val intent = Intent(this, clase)
+        startActivity(intent)
+    }
+
 }
 
 @Composable
---
+++ 02-android/app/src/main/java/com/example/b2023_gr1sw_cjlc/ui/theme/ACicloVida.kt
@@ -0,0 +1,12 @@
+package com.example.b2023_gr1sw_cjlc.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.b2023_gr1sw_cjlc.R
+
+class ACicloVida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aciclo_vida)
+    }
+}
